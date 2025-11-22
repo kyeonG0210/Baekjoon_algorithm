6eb67274a6dbb438725ff079da90529fa3d3dca9
kyeonG0210 <hyeonji413@gmail.com>
2025-11-22 16:08:49 +0900

    [Bronze V] Title: 사칙연산, Time: 44 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.py
@@ -0,0 +1,12 @@
+v1, v2 = input().split(" ")
+v1, v2 = [int(v1), int(v2)]
+plus = v1 + v2
+minus = v1 - v2
+product = v1 * v2
+quotient = v1 // v2
+remainder = v1 % v2
+print(plus)
+print(minus)
+print(product)
+print(quotient)
+print(remainder)
